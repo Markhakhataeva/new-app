fecba3427c79e3a3a44920902f44e0442a447564
mike <mike@hk>
2024-05-03 19:34:23 +0300

    first com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App=()=> {
+    // const array=[22,0,-45,1001,8,6,-0.1,101,2.5,-5555]
+    // const positive=array.filter(number=>number>=0)
+    // const listItem=positive.map((number)=><li>{number}</li>)
+
+
+/////////////////////////////////////////////////
+    // const messages = [
+    //   {message: 'hello', error: true},
+    //   {message: 'javascript', error: false},
+    //   {message: 'lincode', error: true},
+    //   {message: 'react', error: true},
+    //   {message: 'angular', error: false},
+    //   {message: 'es6', error: false},
+    // ];
+    //
+    // const error=messages.filter(mes => mes.error!==true)
+    // console.log(error);
+
+/////////////////////////////////////////////////
+
+    // const words= ['lincode', 'js', 'react', 'css', 'angular', 'bootstrap', 'html'];
+    // const array=words.map(word=>word.length<5 ? '*':word);
+    // console.log(array);
+
+////////////////////////////////////////////////////////
+
+
+    // function sendMessage(message) {
+    //     /*
+    //     * тут какой-то большой код для отправки сообщения
+    //     */
+    // }
+    // const sendMessage=(message)=>{
+    //     return()
+    // }
+
+
+
+    // function half(number) {
+    //     return number / 2;
+    // }
+    // const half=(number)=>{
+    //     return number / 2;
+    // }
+
+
+
+
+    // function showConsole() {
+    //     console.log('lincode');
+    // }
+    //
+    // const showConsole=()=>{
+    //     console.log('lincode');
+    // }
+
+
+
+
+    // function concatWords(first, second) {
+    //     return first + second;
+    // }
+    // const concatWords=(first, second)=>{
+    //     return first + second;
+    // }
+
+//////////////////////////////////////////////////////////
+
+
+    // const justText = () => 'lincode';
+    // function justText(){
+    //     return('lincode');
+    // }
+
+
+
+    // const logging = text => console.log(text);
+    // function logging(text){
+    //     console.log(text)
+    // }
+
+
+
+
+    // const add = (x,y) => {
+    //     const z = 3;
+    //
+    //     return z * x * y;
+    // }
+    // function add(x,y) {
+    //     const z = 3;
+    //
+    //     return z * x * y;
+    // }
+
+
+
+
+
+    // const onlyPositive = (number) => {
+    //     if(number < 0) {
+    //         return false;
+    //     }
+    //
+    //     return true;
+    // }
+    // function onlyPositive(number) {
+    //     if(number < 0) {
+    //         return false;
+    //     }
+    //
+    //     return true;
+    // }
+
+    return(
+      <div className="App">
+          {/*<ul>{listItem}</ul>*/}
+          {/*{array}*/}
+      </div>
+  )
+
+
 }
 
-export default App;
+
